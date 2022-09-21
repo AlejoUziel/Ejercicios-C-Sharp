e980fc7e88a9a1ea6541ab8255eb175416a762b8
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-21 16:02:11 -0600

    Add files via upload

---
+++ EntradaDeDatos/EntradaDeDatos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaDeDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cree un programa que le pida al usuario su nombre y lo guarde en una variable y luego imprima el nombre.
+            Console.WriteLine("cual es tu nombre?");
+            string nombre = Console.ReadLine();  
+            Console.WriteLine("hola! " + nombre);  
+            Console.ReadKey();  
+        }
+    }
+}
