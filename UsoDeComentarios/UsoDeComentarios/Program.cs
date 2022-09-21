589c3356631471f2643bb958b7403226ee0481cd
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-21 10:30:22 -0600

    Add files via upload

---
+++ UsoDeComentarios/UsoDeComentarios/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoDeComentarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* haga uso de los comentarios de una sola linea y de multiples lineas en los siguientes ConsoleWriteLine*/
+
+            Console.WriteLine("hola Mundo"); //  este un comentario de una sola linea
+
+            Console.WriteLine("hola Mundo"); /*  este es un comentario de multiples lineas
+            y puede comentar en varias lineas de codigo sin afectar su codigo*/
+
+
+        }
+    }
+}
