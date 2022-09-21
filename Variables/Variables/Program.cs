44ade10e36c23a4576a93bd2adda0f416a99327d
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-21 12:32:09 -0600

    Add files via upload

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* cree las siguientes variables: int, double, char, string y bool */
+
+            int VariableEntero = 50;
+            double variableTipoDouble = 3.14159265359;
+            char variableTipoFloat = 'A';
+            string variableTipoString = "alejandro";
+            bool variableTipoBool = true;
+
+
+        }
+    }
+}
