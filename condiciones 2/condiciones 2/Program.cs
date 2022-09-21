c7891b9fe3dc663827879d1a7d0ac8116a92eaff
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-21 16:32:08 -0600

    Add files via upload

---
+++ condiciones 2/condiciones 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condiciones_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* cree un programa donde calcule si la nota del alumno es mayor a 70 pasa el año y si la nota esta entre 60 a 70 
+               tiene derecho de ir a recuperacion y si es menor a 60 tiene que repetir el año */
+
+            Console.WriteLine("ingrese la nota del alumno");
+            double nota = Convert.ToDouble(Console.ReadLine()); 
+
+        if(nota>70)
+            {
+                Console.WriteLine("el alumno pasa de año :)");
+                Console.ReadKey();
+            }
+        else if(nota>60)
+            {
+                Console.WriteLine("el alumno tiene derecho a una recuperacion :) ");
+                Console.ReadKey();
+            }
+
+            else
+            {
+                Console.WriteLine("el alumno debe repetir el año :( ");
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
