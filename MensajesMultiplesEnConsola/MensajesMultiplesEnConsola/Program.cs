91d10422fd2e332653e4e5fdf68dbd8ffbbfaaaf
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-21 10:29:40 -0600

    Add files via upload

---
+++ MensajesMultiplesEnConsola/MensajesMultiplesEnConsola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensajesMultiplesEnConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* cree dos mensajes en consola.
+             1. cree el proximo mensaje "Hola"
+             2. cree un segundo mensaje con tu nombre*/
+
+            Console.WriteLine("Hola!");
+            Console.WriteLine("Alejandro");
+            Console.ReadKey();
+
+        }
+    }
+}
