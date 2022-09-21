29427d7e27aa21fb7d0c046e4dac039554077dd9
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-20 20:16:44 -0600

    hola mundo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ejercicio:1
+             cree un mensaje en la consola, el mensaje debe ser: Hola Mundo!  */
+
+            Console.WriteLine("Hola Mundo!"); /* crea un mensaje en la consola */
+            Console.ReadKey(); /* mantiene la consola abierta */
+        }
+    }
+}
