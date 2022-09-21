8e3de8227a7f307f0ae0308b27680bd6ef27a2aa
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-21 15:21:12 -0600

    Add files via upload

---
+++ OperadoresAritmeticos/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresAritmeticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* cree un programa que pida dos numeros y almacenelos en variables y realice las siguientes operaciones:
+             suma, resta, multiplicacion y division y muestre el resultado en la consola. */
+
+            Console.WriteLine("ingrese un numero");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese un numero");
+            double num2= Convert.ToInt32( Console.ReadLine()); 
+
+            double suma= num1 + num2;
+            double resta = num1 - num2;
+            double multiplicacion = num1 * num2;
+            double division = num1 / num2;
+
+            Console.WriteLine("la suma de los dos numeros es:"+ suma);
+            Console.WriteLine("la resta de los dos numeros es:" + resta);
+            Console.WriteLine("la multiplicacion de los dos numeros es:" + multiplicacion);
+            Console.WriteLine("la division de los dos numeros es:" + division);
+            Console.ReadKey();
+        }
+    }
+}
