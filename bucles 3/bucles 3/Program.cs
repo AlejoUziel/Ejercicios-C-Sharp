fc24c156f9a5b768bafc37d9ded1f035c2fd85fa
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-22 13:38:37 -0600

    Add files via upload

---
+++ bucles 3/bucles 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucles_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // haga uso del ciclo For, imprima los numeros del 0 al 4.
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("numero "+i);
+            }
+            Console.ReadKey();
+
+            
+        }
+    }
+}
