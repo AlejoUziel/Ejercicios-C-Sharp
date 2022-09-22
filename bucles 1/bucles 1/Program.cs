8ea1819d981e1c230194acedd4a8b7d31800b5bb
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-21 18:19:28 -0600

    Add files via upload

---
+++ bucles 1/bucles 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucles_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // haga uso del bucle WHILE, Imprima en consola los numeros del 1 al 100.
+
+            int Numero = 0;
+            while(Numero <= 100)
+            {
+                Console.WriteLine(Numero);
+                Numero++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
