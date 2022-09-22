10f5cb27b168eee9b76ca5c3be878d2598794761
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-22 14:53:56 -0600

    Add files via upload

---
+++ bucles 4/bucles 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucles_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // haga uso del ciclo foreach para leer los elementos de la matriz.
+            string[] animales = { "iguana", "Vaca", "Conejo", "capibara" };
+            foreach (string i in animales)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
