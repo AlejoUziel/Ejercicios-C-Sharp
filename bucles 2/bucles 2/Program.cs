7e0e06e8ed4687badb6901dc6dbae8574db29e73
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-22 13:37:53 -0600

    Add files via upload

---
+++ bucles 2/bucles 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucles_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Haga uso del ciclo Do 
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i <= 100);
+            Console.ReadKey();
+        }
+    }
+}
