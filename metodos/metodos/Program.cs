b15a4966e659d9d4b197bf852c92d252fefc6dd0
AlejoUziel <112787990+AlejoUziel@users.noreply.github.com>
2022-09-22 20:04:33 -0600

    Add files via upload

---
+++ metodos/metodos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodos
+{
+    internal class Program
+    {
+        static void MiPrimerMetodo()  // metodo 
+        {
+            Console.WriteLine("Este mensaje esta dentro de un metodo!"); 
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+           MiPrimerMetodo(); // llamada del metodo.
+        }
+    }
+}
